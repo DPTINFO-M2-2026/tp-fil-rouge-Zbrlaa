256ae274f39867e207ef7c187226bb56bf1f39d1
Zbrlaa <shawn.pelerin@gmail.com>
2025-09-25 22:53:13 +0200

    Init env dev

---
+++ src/test/java/fr/utln/spelerin/resources/UserResourceIT.java
@@ -0,0 +1,8 @@
+package fr.utln.spelerin.resources;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class UserResourceIT extends UserResourceTest {
+    // Execute the same tests but in packaged mode.
+}
